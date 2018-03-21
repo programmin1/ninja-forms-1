0d94659a722d57e08dda6d41f726f363a55141bd
Eric Windham <eric.windham@gmail.com>
2018-03-21 13:25:13 -0400

    Issue#3330 (#3429)
    * initial changes to delete all data
    
    * a few changes
    
    * changes to delete ALL data
    
    * finishes issue 3330 by removing submissions and then db tables after deactivating Ninja Forms
    
    * grey out modal buttons on DELETE click
    
    * drop table if exists for 2.9 tables

--- ninja-forms.php
+++ ninja-forms.php
@@ -230,6 +230,7 @@ if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! ( isset( $_POST[ 'nf
                 self::$instance->controllers[ 'preview' ]       = new NF_AJAX_Controllers_Preview();
                 self::$instance->controllers[ 'submission' ]    = new NF_AJAX_Controllers_Submission();
                 self::$instance->controllers[ 'savedfields' ]   = new NF_AJAX_Controllers_SavedFields();
+                self::$instance->controllers[ 'deletealldata' ] = new NF_AJAX_Controllers_DeleteAllData();
                 self::$instance->controllers[ 'jserror' ]       = new NF_AJAX_Controllers_JSError();
 
                 /*
