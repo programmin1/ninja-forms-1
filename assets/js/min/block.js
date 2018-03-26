87d223b654ab28bf4a84d663a8ece4dcfd357b11
Eric Windham <eric.windham@gmail.com>
2018-03-26 14:46:24 -0400

    fix autofilter value issue

--- assets/js/min/block.js
+++ assets/js/min/block.js
@@ -65,8 +65,6 @@
 				var elID = event.target.parentNode.parentNode;
 				var idArray = elID.getAttribute( 'id' ).split( '-' );
 				var nfOptions = document.getElementById( 'nf-filter-container-' + idArray[ idArray.length -1 ] );
-				var inputEl = document.getElementById( 'formFilter-sidebar' );
-				inputEl.value = event.target.innerText;
 				nfOptions.style.display = 'none';
 			}
 
