5c98bb19e1352b6515b0492389ab008d4a8fbf9e
Kevin Stover <kstover@gmail.com>
2018-03-28 12:36:04 -0400

    First pass at Ninja Mail ad.

--- includes/Config/DashboardPromotions.php
+++ includes/Config/DashboardPromotions.php
@@ -8,20 +8,20 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
   |--------------------------------------------------------------------------
   |
   */
-  'exampleA' => [
-    'id' => 'exampleA',
-    'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad+[A]" /></a>'
+  'txnemail' => [
+    'id' => 'txnemail',
+    'content' => '<a href="#services"><span class="dashicons dashicons-email-alt"></span>' . __( 'Want to improve the reliability of your submission emails?', 'ninja-forms' ) . '<br /><span class="cta">' . __( 'Try our new Ninja Mail service!', 'ninja-forms' ) . '</span></a>'
   ],
 
-  /*
-  |--------------------------------------------------------------------------
-  | Exmaple 4
-  |--------------------------------------------------------------------------
-  |
-  */
-  'exampleB' => [
-    'id' => 'exampleB',
-    'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad+[B]" /></a>'
-  ],
+  // /*
+  // |--------------------------------------------------------------------------
+  // | Exmaple 4
+  // |--------------------------------------------------------------------------
+  // |
+  // */
+  // 'exampleB' => [
+  //   'id' => 'exampleB',
+  //   'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad+[B]" /></a>'
+  // ],
 
 ]);
