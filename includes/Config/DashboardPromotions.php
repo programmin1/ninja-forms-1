fcd1080951e5d8d9dad949c78eca793be942a739
Kyle B. Johnson <kyle@wpninjas.com>
2018-03-28 10:51:49 -0400

    Comment out example dashboard promotion.

--- includes/Config/DashboardPromotions.php
+++ includes/Config/DashboardPromotions.php
@@ -4,24 +4,13 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
 
   /*
   |--------------------------------------------------------------------------
-  | Exmaple A
+  | Exmaple
   |--------------------------------------------------------------------------
   |
   */
-  'exampleA' => [
-    'id' => 'exampleA',
-    'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad+[A]" /></a>'
-  ],
-
-  /*
-  |--------------------------------------------------------------------------
-  | Exmaple 4
-  |--------------------------------------------------------------------------
-  |
-  */
-  'exampleB' => [
-    'id' => 'exampleB',
-    'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad+[B]" /></a>'
-  ],
+  // 'example' => [
+  //   'id' => 'example',
+  //   'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad" /></a>'
+  // ],
 
 ]);
