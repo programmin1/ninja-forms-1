e195a4584cceef5a0ea0a875646d9784a38c0678
Kyle B. Johnson <kyle@wpninjas.com>
2018-03-30 14:28:08 -0400

    Add placeholder Ninja Mail promotion.

--- includes/Config/DashboardPromotions.php
+++ includes/Config/DashboardPromotions.php
@@ -4,13 +4,18 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
 
   /*
   |--------------------------------------------------------------------------
-  | Exmaple
+  | Ninja Mail
   |--------------------------------------------------------------------------
   |
   */
-  // 'example' => [
-  //   'id' => 'example',
-  //   'content' => '<a href="#services"><img height="100px" width="750px" style="display:block;margin:auto;" src="https://placehold.it/750x100&text=Services+Ad" /></a>'
-  // ],
+  'ninja-mail' => [
+    'id' => 'ninja-mail',
+    'content' => '
+      <a href="#services">
+        <p style="text-align:center;padding-top:50px;padding-bottom:50px;width:750px;background-color:lightgray;color:white;">
+          Increase Email Deliverability with a dedicated email service by Ninja Forms.
+        </p>
+      </a>',
+  ],
 
 ]);
