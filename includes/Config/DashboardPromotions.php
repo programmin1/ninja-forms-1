0b7df275bc81d69fe09315d6013f7f43ed2411ab
Kyle B. Johnson <kyle@wpninjas.com>
2018-05-01 12:18:16 -0400

    Fix modal popup from banner click.

--- includes/Config/DashboardPromotions.php
+++ includes/Config/DashboardPromotions.php
@@ -13,7 +13,9 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
     'id' => 'ninja-mail',
     'content' => '<a href="#services"><span class="dashicons dashicons-email-alt"></span>' . __( 'Hosts are bad at sending emails. Improve the reliability of your submission emails! ', 'ninja-forms' ) . '<br /><span class="cta">' . __( 'Try our new Ninja Mail service!', 'ninja-forms' ) . '</span></a>',
     'script' => "
-      Backbone.Radio.channel( 'dashboard' ).request( 'more:service:ninja-mail' );
+      setTimeout(function(){ /* Wait for services to init. */
+        Backbone.Radio.channel( 'dashboard' ).request( 'more:service:ninja-mail' );
+      }, 500);
     "
   ],
 
