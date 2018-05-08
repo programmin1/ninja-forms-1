9c8348695f802c45ff2d3c21fa290ff07aed8d66
Kyle B. Johnson <kyle@wpninjas.com>
2018-05-08 10:08:12 -0400

    Update array syntax for php5.2 compatibility.

--- includes/Config/DashboardPromotions.php
+++ includes/Config/DashboardPromotions.php
@@ -1,6 +1,6 @@
 <?php
 
-return apply_filters( 'ninja-forms-dashboard-promotions', [
+return apply_filters( 'ninja-forms-dashboard-promotions', array(
 
   /*
   |--------------------------------------------------------------------------
@@ -9,7 +9,7 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
   |
   */
 
-  'ninja-mail' => [
+  'ninja-mail' => array(
     'id' => 'ninja-mail',
     'content' => '<a href="#services"><span class="dashicons dashicons-email-alt"></span>' . __( 'Hosts are bad at sending emails. Improve the reliability of your submission emails! ', 'ninja-forms' ) . '<br /><span class="cta">' . __( 'Try our new Ninja Mail service!', 'ninja-forms' ) . '</span></a>',
     'script' => "
@@ -17,7 +17,7 @@ return apply_filters( 'ninja-forms-dashboard-promotions', [
         Backbone.Radio.channel( 'dashboard' ).request( 'more:service:ninja-mail' );
       }, 500);
     "
-  ],
+  ),
 
 
-]);
+));
