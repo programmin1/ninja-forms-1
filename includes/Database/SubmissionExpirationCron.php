6622bd8510d7ceede1b9981294db19196a79c74f
Kenny Hall <klhall1987@gmail.com>
2018-06-01 14:06:35 -0400

    Fixed CRON

--- includes/Database/SubmissionExpirationCron.php
+++ includes/Database/SubmissionExpirationCron.php
@@ -13,9 +13,10 @@ final class NF_Database_SubmissionExpirationCron
         $options = get_option( 'nf_sub_expiration' );
 
         // Schedules our CRON job.
-        if( ! wp_next_scheduled( 'expired_submission_cron' ) && ! $options ) {
-            wp_schedule_event( time(), 'daily', $this->expired_submission_cron( $options ) );
+        if( ! wp_next_scheduled( 'nf_submission_expiration_cron' ) &&  ! empty( $options ) ) {
+            wp_schedule_event( time(), 'daily', 'nf_submission_expiration_cron' );
         }
+        add_action( 'nf_submission_expiration_cron', array( $this, 'expired_submission_cron' ) );
     }
 
     /**
