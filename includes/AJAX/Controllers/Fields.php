607b72da2e3bd6485410b0aae6b71a3450381ade
Eric Windham <eric.windham@gmail.com>
2018-06-15 09:39:57 -0400

    added comments

--- includes/AJAX/Controllers/Fields.php
+++ includes/AJAX/Controllers/Fields.php
@@ -11,17 +11,23 @@ class NF_AJAX_Controllers_Fields extends NF_Abstracts_Controller
 
 	}
 
+	/**
+	 * Check if the field has data, if so let the front-end know to show
+	 * delete field modal
+	 */
 	public function maybe_delete_field() {
 		$field_id = $_REQUEST[ 'fieldID' ];
-		$field_key = $_REQUEST[ 'fieldKey' ];
+//		$field_key = $_REQUEST[ 'fieldKey' ];
 
 		global $wpdb;
+		// query for checking postmeta for submission data for field
 		$sql = $wpdb->prepare( "SELECT meta_value FROM `" . $wpdb->prefix . "postmeta` 
 			WHERE meta_key = '_field_%d' LIMIT 1", $field_id );
 		$result = $wpdb->get_results( $sql, 'ARRAY_N' );
 
 		$has_data = false;
 
+		// if there are results, has_data is true
 		if( 0 < count( $result ) ) {
 			$has_data = true;
 		}
