2e9d9544fa4b4e22306a3d475b669ccb3c5c791e
Kenny Hall <klhall1987@gmail.com>
2018-06-21 11:26:07 -0400

    Removed the num_min setting and added a help text to set_expired_subs setting.

--- includes/Config/ActionSaveSettings.php
+++ includes/Config/ActionSaveSettings.php
@@ -60,7 +60,8 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
         'type' => 'toggle',
         'group' => 'advanced',
         'label' => __( 'Set Submissions to expire?', 'ninja-forms' ),
-        'value' => '',
+        'value' => 0,
+        'help'  => __( 'Sets submissions to be trashes after a certain number of days, it affects all existing and new submissions', 'ninja-forms' ),
         'width' => 'one-half',
     ),
 
@@ -74,7 +75,6 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
         'label' => __( 'How long in days until subs expire?', 'ninja-forms' ),
         'value' => '90',
         'width' => 'one-half',
-        'num_min' => 0,
         'deps'  => array(
             'set_subs_to_expire' => 1
         )
