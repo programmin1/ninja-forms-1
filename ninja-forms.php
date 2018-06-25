67136da99f2861e8311b0809ebeaae30fcf0efca
KR Moorhouse <spkr4thded@gmail.com>
2018-06-25 12:39:57 -0400

    Version bumped the core file to ensure database changes work.

--- ninja-forms.php
+++ ninja-forms.php
@@ -58,7 +58,7 @@ if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! ( isset( $_POST[ 'nf
         /**
          * @since 3.0
          */
-        const VERSION = '3.3.7';
+        const VERSION = '3.3.8';
 
         const WP_MIN_VERSION = '4.7';
 
